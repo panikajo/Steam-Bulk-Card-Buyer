0d659609710c84cb2a1990e5977794407e0d212b
Alex Corn <DoctorMcKay@users.noreply.github.com>
2013-10-08 13:24:04 -0400

    Fixed issues with special characters in card names

--- steamcommunity.js
+++ steamcommunity.js
@@ -24,8 +24,8 @@ if(links && $('.unowned').length > 0) {
 		for(var i = 0; i < cards.length; i += increment) { // skip every other one since it's a series number
 			var card = $(cards[i]);
 			var name = $.trim(card.html().replace('<div style="clear: right"></div>', ''));
-			$('#buycardspanel').append('<span class="cardname"><b>' + name + '</b></span> - <span class="cardprice" id="Price-' + name.replace(/"/g, '&quot;').replace(/ /g, '') + '" data-name="' + name.replace(/"/g, '&quot;') + '">Loading...</span>' + '<br />');
-			$.get('/market/listings/753/' + appid + '-' + name, onCardPriceLoaded);
+			$('#buycardspanel').append('<span class="cardname"><b>' + name + '</b></span> - <span class="cardprice" id="Price-' + name2id(name).replace(/"/g, '&quot;') + '">Loading...</span>' + '<br />');
+			$.get('/market/listings/753/' + appid + '-' + encodeURIComponent(name), onCardPriceLoaded);
 		}
 		
 		$('#buycardspanel').show('blind');
@@ -51,7 +51,7 @@ function onCardPriceLoaded(data, textStatus) {
 	var pricenofee = findElementByClass($(item), 'span', 'market_listing_price_without_fee');
 	
 	if(textStatus != 'success' || !item || !price || !pricenofee) {
-		$('#Price-' + name.replace(/ /g, '')).html('Error');
+		$('#Price-' + name2id(name)).html('Error');
 	} else {
 		var pos = data.indexOf('g_sessionID = "') + 15;
 		var pos2 = data.indexOf('"', pos);
@@ -67,9 +67,9 @@ function onCardPriceLoaded(data, textStatus) {
 			return;
 		}
 		
-		cards.push({session: sessionID, listing: listingID, total: totalPrice, theirs: theirPrice, element: '#Price-' + name.replace(/ /g, '')});
+		cards.push({session: sessionID, listing: listingID, total: totalPrice, theirs: theirPrice, element: '#Price-' + name2id(name)});
 		
-		$('#Price-' + name.replace(/ /g, '')).html('$' + totalPrice);
+		$('#Price-' + name2id(name)).html('$' + totalPrice);
 	}
 	
 	if(cards.length == $('.cardprice').length) {
@@ -119,4 +119,74 @@ function buyCard() {
 			buyCard();
 		}
 	});
+}
+
+function name2id(name) {
+	return htmlspecialchars(name.replace(/ /g, '').replace(/:/g, '').replace(/\./g, ''));
+}
+
+function htmlspecialchars(string, quote_style, charset, double_encode) {
+  // http://kevin.vanzonneveld.net
+  // +   original by: Mirek Slugen
+  // +   improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+  // +   bugfixed by: Nathan
+  // +   bugfixed by: Arno
+  // +    revised by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+  // +    bugfixed by: Brett Zamir (http://brett-zamir.me)
+  // +      input by: Ratheous
+  // +      input by: Mailfaker (http://www.weedem.fr/)
+  // +      reimplemented by: Brett Zamir (http://brett-zamir.me)
+  // +      input by: felix
+  // +    bugfixed by: Brett Zamir (http://brett-zamir.me)
+  // %        note 1: charset argument not supported
+  // *     example 1: htmlspecialchars("<a href='test'>Test</a>", 'ENT_QUOTES');
+  // *     returns 1: '&lt;a href=&#039;test&#039;&gt;Test&lt;/a&gt;'
+  // *     example 2: htmlspecialchars("ab\"c'd", ['ENT_NOQUOTES', 'ENT_QUOTES']);
+  // *     returns 2: 'ab"c&#039;d'
+  // *     example 3: htmlspecialchars("my "&entity;" is still here", null, null, false);
+  // *     returns 3: 'my &quot;&entity;&quot; is still here'
+  var optTemp = 0,
+    i = 0,
+    noquotes = false;
+  if (typeof quote_style === 'undefined' || quote_style === null) {
+    quote_style = 2;
+  }
+  string = string.toString();
+  if (double_encode !== false) { // Put this first to avoid double-encoding
+    string = string.replace(/&/g, '&amp;');
+  }
+  string = string.replace(/</g, '&lt;').replace(/>/g, '&gt;');
+
+  var OPTS = {
+    'ENT_NOQUOTES': 0,
+    'ENT_HTML_QUOTE_SINGLE': 1,
+    'ENT_HTML_QUOTE_DOUBLE': 2,
+    'ENT_COMPAT': 2,
+    'ENT_QUOTES': 3,
+    'ENT_IGNORE': 4
+  };
+  if (quote_style === 0) {
+    noquotes = true;
+  }
+  if (typeof quote_style !== 'number') { // Allow for a single string or an array of string flags
+    quote_style = [].concat(quote_style);
+    for (i = 0; i < quote_style.length; i++) {
+      // Resolve string input to bitwise e.g. 'ENT_IGNORE' becomes 4
+      if (OPTS[quote_style[i]] === 0) {
+        noquotes = true;
+      }
+      else if (OPTS[quote_style[i]]) {
+        optTemp = optTemp | OPTS[quote_style[i]];
+      }
+    }
+    quote_style = optTemp;
+  }
+  if (quote_style & OPTS.ENT_HTML_QUOTE_SINGLE) {
+    string = string.replace(/'/g, '&#039;');
+  }
+  if (!noquotes) {
+    string = string.replace(/"/g, '&quot;');
+  }
+
+  return string;
 }
